29ef06647603d9556029295a1c70ccd9cf2db69f
Prabhakar <prabhakarkr802@gmail.com>
2021-07-02 17:51:34 +0530

    Resources_WE(Assignment MASAI School)

--- app/src/main/java/com/prabhakar/resources_we/MainActivity.java
+++ app/src/main/java/com/prabhakar/resources_we/MainActivity.java
@@ -1,8 +1,8 @@
 package com.prabhakar.resources_we;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
